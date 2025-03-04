e927dcdc47d486783e53b95c372892ec8bdb0165
powerSeries <joseesotolongo@gmail.com>
2025-03-03 20:42:17 -0500

    Added loading page for when app is open

---
+++ TabooReloaded.Shared/Utils/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabooReloaded.Shared.Utils
+{
+    public static class Constants
+    {
+        public const int LOADING_TIME = 5000;
+    }
+}
